7c697a618951aee169cfe13399f03fabf758f431
arafa <arafa.sco@gmail.com>
2026-05-08 16:54:57 +0000

    build: make estate property modul

---
+++ custom_addons/estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'ara fa adri',
+    'category': 'App',
+    'description': """This is a real estate management module.""",
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/estate_property.xml',
+    ]
+}
---
+++ custom_addons/estate/models/estate_property.py
@@ -0,0 +1,23 @@
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Estate Property"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available From")
+    expected_price = fields.Float(string="Expected Price")
+    selling_price = fields.Float(string="Selling Price")
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+    garden_orientation = fields.Selection(
+        string="Garden Orientation",
+        selection=[("N", "North"), ("S", "South"), ("E", "East"), ("W", "West")],
+    )
+
