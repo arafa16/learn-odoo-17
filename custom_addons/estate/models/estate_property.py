dfe60da9ce188e7c34b229247e16ae89e6dad4e1
arafa <arafa.sco@gmail.com>
2026-05-09 05:13:57 +0000

    build: make computed fields and onchange in the estate form

--- custom_addons/estate/models/estate_property.py
+++ custom_addons/estate/models/estate_property.py
@@ -19,6 +19,20 @@ class EstateProperty(models.Model):
             if record.selling_price < 1:
                 raise ValidationError("Selling price cannot be negative or 0.")
 
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for prop in self:
+            prop.total_area = prop.living_area + prop.garden_area
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "N"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
+
     name = fields.Char(string="Title", required=True)
     description = fields.Text(string="Description", default="No description provided.")
     postcode = fields.Char(string="Postcode")
@@ -36,4 +50,7 @@ class EstateProperty(models.Model):
         selection=[("N", "North"), ("S", "South"), ("E", "East"), ("W", "West")],
     )
     last_seen = fields.Datetime(string="Last Seen", default=fields.Datetime.now)
+    user_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
+    buyer_id = fields.Many2one("res.partner", string="Buyer", readonly=True, copy=False)
+    total_area = fields.Integer(readonly=True, compute="_compute_total_area")
 
