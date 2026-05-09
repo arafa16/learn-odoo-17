644b4d84d33fbfee6d0b7f8ae143eb239ad47b7b
arafa <arafa.sco@gmail.com>
2026-05-09 04:50:08 +0000

    build: make sql constraints and api constrains

--- custom_addons/estate/models/estate_property.py
+++ custom_addons/estate/models/estate_property.py
@@ -1,15 +1,30 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
+
+    _sql_constraints = [
+        (
+            'unique_name',
+            'UNIQUE(name)',
+            'The property name must be unique.'
+        )
+    ]
+
+    @api.constrains('selling_price')
+    def _check_selling_price(self):
+        for record in self:
+            if record.selling_price < 1:
+                raise ValidationError("Selling price cannot be negative or 0.")
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text(string="Description", default="No description provided.")
     postcode = fields.Char(string="Postcode")
     date_availability = fields.Date(string="Available From", default=fields.Datetime.now)
     expected_price = fields.Float(string="Expected Price")
-    selling_price = fields.Float(string="Selling Price", default=1000000)
+    selling_price = fields.Float(default=1000000)
     bedrooms = fields.Integer(string="Bedrooms")
     living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer(string="Facades")
