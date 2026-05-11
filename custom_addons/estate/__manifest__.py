f7ba4bce677bb564d494656e2445cad8b6a1510f
arafa <arafa.sco@gmail.com>
2026-05-11 08:25:01 +0000

    build: Load data from csv file

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -7,8 +7,15 @@
     'description': """This is a real estate management module.""",
     'application': True,
     'data': [
+
+        # security
         'security/ir.model.access.csv',
+
+        # views
         'views/menu.xml',
         'views/estate_property.xml',
+
+        # load initial data
+        'data/estate.property.csv',
     ]
 }
